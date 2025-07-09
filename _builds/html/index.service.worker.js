ffa9ae890efa6ab4a56965bc2370f5d2cd8e7ed9
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2025-07-10 01:20:55 +0200

    build html

--- _builds/html/index.service.worker.js
+++ _builds/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751839904|241529071';
+const CACHE_VERSION = '1752103249|10762850888';
 /** @type {string} */
 const CACHE_PREFIX = 'tower game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
